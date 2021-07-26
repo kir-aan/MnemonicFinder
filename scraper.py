779ee729df85968d624bd03f770ca7ce5c4c23f1
Kiran <sai8kiran99@gmail.com>
2021-07-26 11:42:43 +0530

    fixed bugs

--- scraper.py
+++ scraper.py
@@ -2,24 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+def findMnemonic(word):
+    link = 'https://mnemonicdictionary.com/?word=' + word
+    page = requests.get(link)
+    txt = page.text
+    status = page.status_code
+    soup = BeautifulSoup(page.content,'html5lib')
+    page_body = soup.body
+    mnemonicDiv = soup.find('div',attrs={'class':'card-text'})
+    mnemonic = ''
+    for w in mnemonicDiv.find('p').text.split():
+        mnemonic = mnemonic + w + ' '
+    print(mnemonic)
+
+    line = {'word':word, 'mnemonic':mnemonic}
+    filename = 'mnemonicDictionary.csv'
+    with open(filename,'a', newline='') as f:
+        w = csv.DictWriter(f,fieldnames=['word','mnemonic'])
+        w.writerow(line)
+        f.close()
+
+
 print("Enter the word you want to find mnemonic for: ",end='')
 word = input()
-link = 'https://mnemonicdictionary.com/?word=' + word
-page = requests.get(link)
-txt = page.text
-status = page.status_code
-soup = BeautifulSoup(page.content,'html5lib')
-page_body = soup.body
-mnemonicDiv = soup.find('div',attrs={'class':'card-text'})
-mnemonic = ''
-for w in mnemonicDiv.find('p').text.split():
-    mnemonic = mnemonic + w + ' '
-print(mnemonic)
+findMnemonic(word)
 
-line = {'word':word, 'mnemonic':mnemonic}
-filename = 'mnemonicDictionary.csv'
-with open(filename,'w', newline='') as f:
-    w = csv.DictWriter(f,['word','mnemonic'])
-    w.writeheader()
-    w.writerow(line)
-    
+while(True):
+    next = input("Do you want to find mnemonic for another word?(Y/N): ")
+    if(next=="N"):
+        break
+    print("Enter the word you want to find mnemonic for: ",end='')
+    word = input()
+    findMnemonic(word)
